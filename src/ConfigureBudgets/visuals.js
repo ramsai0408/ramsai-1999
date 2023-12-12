07fa3d6aca57dfa3d4266a5eb3ded9d2357181ab
ramsai0408 <ramsainimmagadda1999@gmail.com>
2023-12-12 17:43:54 -0500

    backendipexpense

--- src/ConfigureBudgets/visuals.js
+++ src/ConfigureBudgets/visuals.js
@@ -85,7 +85,7 @@ function Visuals() {
 
     if (selectedMonth) {
       axios
-        .get(`http://localhost:3002/get-expenses/${userId}?month=${selectedMonth}`, {
+        .get(`http://174.138.56.121:3002/get-expenses/${userId}?month=${selectedMonth}`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
